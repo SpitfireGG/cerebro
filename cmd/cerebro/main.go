b600b0e8cf7d0f8debb902e6472e317431c861ae
SpitfireGG <raambasnet02@gmail.com>
2025-07-06 10:21:02 +0545

    mv auth->auth.go

--- cmd/cerebro/main.go
+++ cmd/cerebro/main.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spitfiregg/cerebro/cmd/auth"
+	"github.com/spitfiregg/cerebro/internal/bubble"
+	"github.com/spitfiregg/cerebro/internal/debug"
 	"log"
 	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/joho/godotenv"
-	api "github.com/spitfiregg/garlic/internal/api/gemini"
-	"github.com/spitfiregg/garlic/internal/bubble"
-	"github.com/spitfiregg/garlic/internal/debug"
 )
 
 // TODO: add feat:  initiate multiple reponses with concurrency
 
 func main() {
 
-	// load the environment variable
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("could not find .env")
-	}
-	gemini_key := os.Getenv("GEMINI_API_KEY")
-	if gemini_key == "" {
-		fmt.Println("key was not set or found")
-		os.Exit(1)
-	}
-
-	appCfg := api.NewDefaultAppConfig(gemini_key)
-	model := bubble.InitialModel(appCfg)
+	model := bubble.InitialModel(auth.LoadAPiKey())
 
 	var dbgModel debug.Debug
 	dbgModel.EnterDebug("", "debug.log")
